8eb82f6400ad70a6cb9511e39ae626f3320bf792
Elin Stridsberg <elin-stridsberg@hotmail.com>
2024-04-03 12:40:27 +0200

    buttons done

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,10 +1,73 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from "react"
 
 export const Homepage = () => {
+    const [customer, setCustomer] = useState<string>("")
 
-    return (
-        <>            
-        <div className="homeContainer">
+    useEffect(() => {
+        const authorize = async () => {
+            const response = await fetch("http://localhost:3001/api/auth/authorize", {
+                credentials: "include"
+     
+             }) 
+             const data = await response.json()
+             if (response.status === 200) {
+                setCustomer(data)
+             } else {
+                setCustomer("")
+             }
+        }
+        authorize()
+    }, [])
+    const register = async() => {
+        const response = await fetch("http://localhost:3001/api/auth/register", {
+           method:  "POST",
+           headers: {
+            "Content-Type" : "application/json"
+           },
+           body: JSON.stringify({email: "abc.com@jdw", password: "56789"})
+        }) 
+
+        const data = await response.json()
+        console.log(data)
+    }
+       // vi gör en knapp til vår funktion. Den gör ett anrop till vår server och skickar med data.
+    
+    const login = async () => {
+        const response = await fetch("http://localhost:3001/api/auth/login", {
+            method:  "POST",
+            headers: {
+             "Content-Type" : "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify({email: "abc.com@jdw", password: "56789"})
+           
+         }) 
+         const data = await response.json()
+         if (response.status === 200) {
+            setCustomer(data)
+         } else {
+            setCustomer("")
+         }
+     }
+    const logout = async () => {
+    const response = await fetch("http://localhost:3001/api/auth/logout", {
+           method:  "POST",
+            credentials: "include"          
+        }) 
+
+        if( response.status === 200) {
+            setCustomer("")
+        }
+    }
+       return (
+        <>       
+            <h1>{customer ? "inloggad som: " + customer : "Utloggad"}</h1>
+         
+            <button onClick={register}> Registrera </button>
+            <button onClick={login}> Logga in </button>
+            <button onClick={logout}> Logga ut </button>
+     
+        {/* <div className="homeContainer">
                 <h1>HomePage</h1>
             <div className="buttonContainer">
             <Link to="/login">
@@ -14,7 +77,7 @@ export const Homepage = () => {
                 <button>Register</button>
             </Link>
            </div> 
-        </div>
+        </div> */}
         </>
     )
 }
